98455fad3cfe95a4fd868ea77fc5a844a6242820
Nishant Jain <nishantjainyo@gmail.com>
2022-08-05 14:43:30 +0530

    update auth

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,17 @@
 const validate = async (decoded, request) => {
+  
     if (decoded) {
-      return { isValid: true };
+      if(decoded.isSuperAdmin){
+        return { isValid:true, credentials: 'superAdmin'}
+      }
+
+      if(!decoded.isSuperAdmin && !decoded.isCustomer){
+        return { isValid:true, credentials: 'admin'}
+      }
+
+      if(decoded.isCustomer){
+        return { isValid:true, credentials: 'customer'}
+      }
     } else {
       return { isValid: false };
     }
